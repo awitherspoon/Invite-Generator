3d3b3f0003c57d2818dd1f322b3359b120584793
Andrew Bethel <andrew@striking.ly>
2016-01-11 18:24:59 -0600

    log error when non-json url passed

--- modules/getCustomers.js
+++ modules/getCustomers.js
@@ -3,11 +3,15 @@ var request = require('request')
 function getCustomers (fileLoc, cb) {
   request(fileLoc, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      var resp = body.split('\n')
-      var customerList = resp.map(function (row) {
-        return JSON.parse(row)
-      })
-      cb(customerList)
+      try {
+        var resp = body.split('\n')
+        var customerList = resp.map(function (row) {
+          return JSON.parse(row)
+        })
+        cb(customerList)
+      } catch (error) {
+        console.error('Cannot parse file, please ensure file url is a valid customer list that can be parsed in to JSON')
+      }
     } else {
       console.log('Customers not found')
     }
